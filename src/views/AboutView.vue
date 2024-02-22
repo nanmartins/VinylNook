aecafe190c9fedf307649212a81ddc0ef137a42a
Renan Martins <renanmartins989@gmail.com>
2024-02-22 12:05:01 +0000

    basic content for about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,32 @@
   <div class="about">
     <h2>about</h2>
   </div>
+
+  <div class="about-content-container">
+    <div>
+      <p>Welcome to Vinyl Nook, your haven for exploring the rich world of vinyl albums. At Vinyl Nook, we're passionate about preserving the timeless allure of vinyl records and providing enthusiasts with a platform to delve into the magic of analog sound.</p>
+    </div>
+
+    <div>
+      <h4>Our Mission</h4>
+      <p>Vinyl Nook was born out of a love for music and a desire to create a space where users can uncover, discover, and rediscover their favorite albums in the purest form. Our mission is simple: to connect music lovers with exceptional vinyl records and foster a community where the joy of vinyl collecting and listening is celebrated.</p>
+    </div>
+
+    <div>
+      <h4>Explore, Discover, and Rediscover</h4>
+      <p>Whether you're a seasoned collector or a newcomer to the vinyl scene, Vinyl Nook is your go-to destination for all things vinyl. Dive into our extensive database of albums spanning genres, eras, and artists. From classic rock to jazz, hip-hop to indie, our curated collection offers something for every musical palate.</p>
+    </div>
+
+    <div>
+      <h4>Discover New Gems</h4>
+      <p>Seeking recommendations for your next vinyl purchase or looking to expand your musical horizons? Let Vinyl Nook be your guide. Our recommendation feature analyzes your listening preferences and suggests handpicked vinyl albums that we believe you'll love. Say goodbye to endless scrolling and hello to your new favorite record.</p>
+    </div>
+
+    <div>
+      <h4>Start Your Vinyl Journey Today</h4>
+      <p>Ready to embark on a journey through the enchanting world of vinyl? Join us at Vinyl Nook and immerse yourself in a universe where music comes to life in its purest form. Whether you're seeking to build your collection, explore new genres, or simply bask in the warm, crackling embrace of analog sound, Vinyl Nook is here to accompany you every step of the way.</p>
+    </div>
+  </div>
 </template>
 
 
@@ -19,6 +45,30 @@ h2 {
   line-height: 35%;
   font-weight: 800;
   z-index: -1;
-  /* padding-top: 80px; */
+}
+
+h4 {
+  font-size: 28px;
+  letter-spacing: 2px;
+}
+
+p {
+  font-size: 20px;
+  letter-spacing: 1px;
+}
+
+.about-content-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  background: white;
+  width: 100%;
+  max-width: 1000px;
+  margin: 20px auto;
+  padding: 40px 60px;
+  z-index: 100;
+  border: 1px solid black;
+  border-radius: 2px;
 }
 </style>
